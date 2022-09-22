1b6bd4d45d48a86e04b79c27aea438e5ce47a7aa
RAMMACHyc <sadik.ram12@gmail.com>
2022-09-22 16:45:00 +0100

    projet de fin sas gestion de pharmacie

--- main.c
+++ main.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+void selection(int *T,int n)
+{
+int j,min,perm,i,;
+  for(i=0;i<n-1;i++)
+    {
+        min=i;
 
+    for(j=i+1;j<n;j++){
+        if (T[min]>T[j])
+      min = j;
+    }
+    if (min!=i){
+       perm = T[i];
+       T[i] = T[min];
+       T[min] = perm;
+       }
+    }
+}
 int main()
 {
-    float A, B;
-    char operateur;
-    printf ("veuillez entrer la valeur de A est : ");
-    scanf("%f",&A);
-    printf("veuillez choisir l'operateur : ");
-    scanf(" %c",&operateur);
-    printf("veuillez entrer la valeur de B est : ");
-    scanf("%f", &B);
-
-
-    switch(operateurg){
-       case '+' :
-            printf("A + B = %.2f",A + B);
-                 break;
-       case '-' :
-            printf("A - B = %.2f", A - B);
-                 break;
-       case '*' :
-            printf("A * B = %.2f" , A * B);
-                 break;
-       default :
-            printf("kolchi khata2\n");
-
-
+    int min,perm,j,i,max;
+    int n;
+    int T[100];
+    printf("veuillez entrer la valeur de n :");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("T[%d]=",i);
+        scanf("%d",&T[i]);
     }
-
-    return 0;
-}
+    selection(*T,n);
